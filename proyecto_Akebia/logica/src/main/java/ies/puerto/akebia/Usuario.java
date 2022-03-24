8ba0114109935e7a815beb59b843c0f8a27a62eb
Christian Novo Sacramento <christianivh366@gmail.com>
2022-03-24 19:16:41 +0000

    Se sube la implementacion del usuario del diagrama de clases

---
+++ proyecto_Akebia/logica/src/main/java/ies/puerto/akebia/Usuario.java
@@ -0,0 +1,87 @@
+package ies.puerto.akebia;
+
+public class Usuario {
+
+    private int id;
+    private String alias;
+    private String email;
+    private String contrasenia;
+
+    /**
+     * Constructor por defecto sin parametros
+     */
+    public Usuario(){}
+
+    /**
+     * Constructor que recibe 3 parametros
+     * @param alias del usuario
+     * @param email del usuario
+     * @param contrasenia del usuario
+     */
+    public Usuario(String alias, String email, String contrasenia) {
+        this.alias = alias;
+        this.email = email;
+        this.contrasenia = contrasenia;
+    }
+
+    //Metodos
+
+    /**
+     * Metodo que sirve para iniciar sesion
+     */
+    public void iniciarSesion(){}
+
+    /**
+     * Metodo que permite ver el perfil del usuario
+     */
+    public void verPerfil(){}
+
+    /**
+     * Metodo que permite cambiar el alias
+     */
+    public void cambiarAlias(){}
+
+    /**
+     * Metodo que permite cambiar el email
+     */
+    public void cambiarEmail(){}
+
+    /**
+     * Metodo que permite cambiar la contrasenia
+     */
+    public void cambiarContrasenia(){}
+
+    // Getters y setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+}
