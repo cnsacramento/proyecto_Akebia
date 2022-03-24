750818be5947b2288b08721e41a9b6e2f620831f
Christian Novo Sacramento <christianivh366@gmail.com>
2022-03-24 19:44:54 +0000

    Se agrega la clase Alimento #11

---
+++ proyecto_Akebia/logica/src/main/java/ies/puerto/akebia/Alimento.java
@@ -0,0 +1,58 @@
+package ies.puerto.akebia;
+
+public class Alimento {
+
+    private int id;
+    private String nombre;
+    private double precio;
+    private Categoria categoria;
+
+    /**
+     * Constructor por defecto sin parametros
+     */
+    public Alimento(){}
+
+    /**
+     * Constructor que recibe 3 parametros
+     * @param nombre del alimento
+     * @param precio del alimento
+     * @param categoria a la cual pertenece el alimento
+     */
+    public Alimento(String nombre, double precio, Categoria categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
